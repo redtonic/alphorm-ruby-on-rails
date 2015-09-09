328f3334c2ea18b5dfa7f69cf06f4566dc28f456
yacine petitprez <formation@redtonic.net>
2015-09-09 06:10:41 +0200

    Apres validateurs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def errors_block_for resource
+		if !resource.errors.messages.empty?
+			render partial: "partials/errors", locals: {
+				errors: resource.errors.messages
+			}
+		end
+	end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,12 @@ class Article < ActiveRecord::Base
   # category: Category
   # author: User
 
+  validates :category, presence: true
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+
   belongs_to :category
   belongs_to :author, class_name: "User"
+
 end
